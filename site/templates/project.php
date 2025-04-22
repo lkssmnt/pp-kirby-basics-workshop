9e6581e1e96e3765ed5db3fc1657f3871d26c018
lkssmnt <hello@lsiemoneit.de>
2025-04-22 15:05:27 +0200

    added project template

---
+++ site/templates/project.php
@@ -0,0 +1,10 @@
+<h1><?= $page->title() ?></h1>
+<h2><?= $page->author() ?></h2>
+
+<p>Developed in <?= $page->location() ?></p>
+
+<?php foreach($page->slideshow()->toFiles() as $slideshowImage): ?>
+  <img src="<?= $slideshowImage->url() ?>">
+<?php endforeach ?>
+
+<?= $page->text() ?>
